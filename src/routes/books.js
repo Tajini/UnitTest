6e1bdcf76d0732d92f4a23acf9d75e4701136d89
Tajini <Sinso7Iblis@hotmail.fr>
2019-02-22 16:24:38 +0100

    test unitaires

--- src/routes/books.js
+++ src/routes/books.js
@@ -5,18 +5,18 @@ import uuid from 'uuid/v4';
 import initIfFileIfMissing from '../utils/initIfFileMissing';
 
 const getBooks = (req, res) => {
-  res.status(200).send({ message: 'route non activé' });
+//  res.status(200).send({ message: 'route non activé' });
 
-  // const pathBooks = path.join(__dirname, '../data/books.json');
-  // fs.readFile(pathBooks, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.status(400).send({ message: 'error fetching books' });
-  //   } else {
-  //     console.log(data);
-  //     res.status(200).send(JSON.parse(data));
-  //   }
-  // });
+   const pathBooks = path.join(__dirname, '../data/books.json');
+   fs.readFile(pathBooks, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ message: 'error fetching books' });
+    } else {
+       console.log(data);
+       res.status(200).send(JSON.parse(data)); //Il a réussi à lire les données
+     }
+   });
 };
 
 /*
@@ -28,140 +28,140 @@ const initialStructure = {
 };
 
 const postBook = (req, res) => {
-  res.status(200).send({ message: 'route non activé' });
-
-  // const pathBooks = path.join(__dirname, '../data/books.json');
-  // initIfFileIfMissing(pathBooks, initialStructure);
-  // fs.readFile(pathBooks, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.status(400).send({ message: 'an Error occured' });
-  //   } else {
-  //     console.log(data);
-  //     let obj;
-  //     if (!data) {
-  //       obj = obj = { books: [] };
-  //     } else {
-  //       obj = JSON.parse(data); //now it an object
-  //     }
-  //     if (!obj || !obj.books) obj = { books: [] };
-  //     obj.books.push({
-  //       id: uuid(),
-  //       title: req.body.title,
-  //       years: req.body.years,
-  //       pages: req.body.pages
-  //     }); //add some data
-  //     const json = JSON.stringify(obj); //convert it back to json
-  //     fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
-  //       if (err) {
-  //         res.status(400).send({ message: 'error adding the book' });
-  //       } else {
-  //         res.status(200).send({ message: 'book successfully added' });
-  //       }
-  //     });
-  //   }
-  // });
-};
+  // res.status(200).send({ message: 'route non activé' });
+ 
+   const pathBooks = path.join(__dirname, '../data/books.json');
+  initIfFileIfMissing(pathBooks, initialStructure);
+   fs.readFile(pathBooks, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ message: 'an Error occured' });
+   } else {
+      console.log(data);
+       let obj; //Création obj
+       if (!data) {  //Si pas de données créer un obj contenant un tableau de livres
+        obj = obj = { books: [] };
+      } else {
+         obj = JSON.parse(data); //now it an object 
+      }
+    if (!obj || !obj.books) obj = { books: [] }; //Si pas d'objets ou pas de livres création d'un tableau de livres
+       obj.books.push({ //Push les bouquins avec les informations contenues dedans (ID, titre, année, nbr de pages)
+        id: uuid(),
+         title: req.body.title,
+        years: req.body.years,
+        pages: req.body.pages
+      }); //add some data
+      const json = JSON.stringify(obj); //convert it back to json
+       fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
+         if (err) {
+         res.status(400).send({ message: 'error adding the book' }); //Le livre n'est pas passé
+        } else {
+          res.status(200).send({ message: 'book successfully added' }); //Le livre est passé
+        }
+      });
+     }
+   }); 
+}; 
 
 /*
  * GET /book/:id route to retrieve a book given its id.
  */
 const getBook = (req, res) => {
-  res.status(200).send({ message: 'route non activé' });
+  // res.status(200).send({ message: 'route non activé' });
 
-  // const pathBooks = path.join(__dirname, '../data/books.json');
-  // fs.readFile(pathBooks, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.status(400).send({ message: 'an Error occured' });
-  //   } else {
-  //     let obj = JSON.parse(data); //now it an object
-  //     const book = obj.books.find(element => {
-  //       if (element.id === req.params.id) return element;
-  //     });
-  //     if (!book) {
-  //       return res.status(400).send({ message: 'book does not exist' });
-  //     }
-  //     console.log(req.params.id);
-  //     console.log(book);
-  //     res.status(200).send({ message: 'book fetched', book });
-  //   }
-  // });
+  const pathBooks = path.join(__dirname, '../data/books.json');
+  fs.readFile(pathBooks, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ message: 'an Error occured' });
+    } else {
+      let obj = JSON.parse(data); //now it an object
+      const book = obj.books.find(element => {
+        if (element.id === req.params.id) return element;
+      });
+      if (!book) {
+        return res.status(400).send({ message: 'book does not exist' }); //Le livre n'existe pas
+      }
+      console.log(req.params.id);
+      console.log(book);
+      res.status(200).send({ message: 'book fetched', book }); //Le livre est arrivé
+    }
+  });
 };
 
 /*
  * DELETE /book/:id to delete a book given its id.
  */
 const deleteBook = (req, res) => {
-  res.status(200).send({ message: 'route non activé' });
+  // res.status(200).send({ message: 'route non activé' });
 
-  // const pathBooks = path.join(__dirname, '../data/books.json');
-  // fs.readFile(pathBooks, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.status(400).send({ message: 'an Error occured' });
-  //   } else {
-  //     let obj = JSON.parse(data); //now it an object
-  //     const bookIndex = obj.books.findIndex(element => {
-  //       if (element.id === req.params.id) {
-  //         return element;
-  //       }
-  //     });
-  //     console.log(bookIndex);
-  //     if (bookIndex === -1) {
-  //       return res.status(400).send({ message: 'book does not exist' });
-  //     }
-  //     obj.books.splice(bookIndex, 1);
-  //     const json = JSON.stringify(obj);
-  //     fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
-  //       if (err) {
-  //         return res.status(400).send({ message: 'error deleting the book' });
-  //       } else {
-  //         return res.status(200).send({ message: 'book successfully deleted' });
-  //       }
-  //     });
-  //   }
-  // });
+  const pathBooks = path.join(__dirname, '../data/books.json');
+  fs.readFile(pathBooks, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ message: 'an Error occured' });
+    } else {
+      let obj = JSON.parse(data); //now it an object
+      const bookIndex = obj.books.findIndex(element => {
+        if (element.id === req.params.id) {
+          return element;
+        }
+      });
+      console.log(bookIndex);
+      if (bookIndex === -1) {
+        return res.status(400).send({ message: 'book does not exist' });
+      }
+      obj.books.splice(bookIndex, 1);
+      const json = JSON.stringify(obj);
+      fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
+        if (err) {
+          return res.status(400).send({ message: 'error deleting the book' });
+        } else {
+          return res.status(200).send({ message: 'book successfully deleted' });
+        }
+      });
+    }
+  });
 };
 
 /*
  * PUT /book/:id to updatea a book given its id
  */
 const updateBook = (req, res) => {
-  res.status(200).send({ message: 'route non activé' });
+  // res.status(200).send({ message: 'route non activé' });
 
-  // const pathBooks = path.join(__dirname, '../data/books.json');
-  // fs.readFile(pathBooks, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.status(400).send({ message: 'an Error occured' });
-  //   } else {
-  //     let obj = JSON.parse(data); //now it an object
-  //     const bookIndex = obj.books.findIndex(element => {
-  //       if (element.id === req.params.id) {
-  //         return element;
-  //       }
-  //     });
-  //     console.log(bookIndex);
-  //     if (bookIndex === -1) {
-  //       return res.status(400).send({ message: 'book does not exist' });
-  //     }
-  //     obj.books.splice(bookIndex, 1, {
-  //       ...obj.books[bookIndex],
-  //       title: req.body.title,
-  //       years: req.body.years,
-  //       pages: req.body.pages
-  //     });
-  //     const json = JSON.stringify(obj);
-  //     fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
-  //       if (err) {
-  //         return res.status(400).send({ message: 'error updating the book' });
-  //       } else {
-  //         return res.status(200).send({ message: 'book successfully updated' });
-  //       }
-  //     });
-  //   }
-  // });
+  const pathBooks = path.join(__dirname, '../data/books.json');
+  fs.readFile(pathBooks, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ message: 'an Error occured' });
+    } else {
+      let obj = JSON.parse(data); //now it an object
+      const bookIndex = obj.books.findIndex(element => {
+        if (element.id === req.params.id) {
+          return element;
+        }
+      });
+      console.log(bookIndex);
+      if (bookIndex === -1) {
+        return res.status(400).send({ message: 'book does not exist' });
+      }
+      obj.books.splice(bookIndex, 1, {
+        ...obj.books[bookIndex],
+        title: req.body.title,
+        years: req.body.years,
+        pages: req.body.pages
+      });
+      const json = JSON.stringify(obj);
+      fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
+        if (err) {
+          return res.status(400).send({ message: 'error updating the book' });
+        } else {
+          return res.status(200).send({ message: 'book successfully updated' });
+        }
+      });
+    }
+  });
 };
 
 //export all the functions
--- src/test/book.js
+++ src/test/book.js
@@ -29,22 +29,268 @@ chai.use(chaiAsPromised);
 
 // TEST UNITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIRE 
 // TEST UNITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIRE 
-describe('localStorage', () => {
-    beforeEach(() => localStorage.clear())
+/* GET
+Que la réponse ait un status 200 
+Que la clé books de la réponse simulé soit un array 
+Que la réponse soi la bonne IDBCursor
 
-    chai
-    .request(server)
-    .get('/book')
-    .end((err, res) => {
-      if (err) console.log(err);
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.a('object');
-      expect(res.body.books).to.be.a('array');
-      expect(res.body.books.length).to.equal(0);
-      done();
+/POST
+Que la réponse ait un status 200 
+Que la clé message de la réponse simulé soit :  ‘book successfully added’ 
+/PUT 
+Que la réponse ait un status 200 
+Que la clé message de la réponse simulé soit :  ‘book successfully updated’ 
+/DELETE
+
+Que la réponse ait un status 200 
+Que la clé message de la réponse simulé soit :  ‘book successfully deleted’
+  */
+
+ 
     
-    
-})});
+describe('Test unity(Totu va bene)', () => {
+
+  beforeEach(()=>{ //Avant chaque test on clean tout ce qui a été intercepté par nock
+      nock.cleanAll()
+})
+
+it('Devrait envoyer le status 200 et un tableau si le tableau de livres est vide',done=>{
+let emptyBooks = { //On instancie un tableau de livres
+  books: []
+}
+
+nock("http://localhost:8080") 
+.get('/book') //Interception des données du Get
+.reply(200,emptyBooks) //Envoie d'un statut de la liste de livres
+chai
+.request("http://localhost:8080") //Récupération des infos avec Chai
+.get('/book')
+.end((err, res) => { //Fin de la fonction, on s'attend à récupérer : Statut 200, un objet, un tableau et une liste de livres vide  
+  if (err) console.log(err);
+  expect(res).to.have.status(200);
+  expect(res.body).to.be.a('object');
+  expect(res.body.books).to.be.a('array');
+expect(res.body.books.length).to.equal(0);
+done();
+});
+}) 
+
+it('Devrait envoyer status 200 et un message si le livre passe',done=>{
+  let message = {
+      message: 'Je crois ton livre il est passé' //Instanciation du message
+  }
+  nock("http://localhost:8080")
+  .post('/book')
+  .reply(200,message) //On envoie le status et le message
+      chai
+      .request('http://localhost:8080')
+      .post('/book') //On récupère le POST
+      .send({ //On envoie un livre 
+          "id": "49657845666",
+          "title": "Si un poney rencontre un chien que se passe t'il ?",
+          "years": "2012",
+          "pages": "21"
+      })
+      .end((err, res) => {
+          if (err) console.log(err);
+          expect(res).to.have.status(200); //On vérifie le statue ok, l'objet , que le message soi bien une ficelle et que le message est le même 
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.be.a('string');
+          expect(res.body.message).to.equal('Je crois ton livre il est passé');
+          done();
+});
+})
+
+
+it('Devrait envoyer un status 200 et un message si la mise à jour fonctionne',done=>{
+  let message = {
+      message: 'Livre mis à jour' //Message : Mise à jour du livre 
+  }
+  nock("http://localhost:8080")
+  .put('/book/49657845666') //On intercepte le livre avec l'ID correspondante 
+  .reply(200,message)
+      chai
+      .request('http://localhost:8080')
+      .put('/book/49657845666') //On récupère les données du livre pour les modifier
+      .send({
+          "pages": "666" //On renvoie une modification apportée au livre
+      })
+      .end((err, res) => {
+          if (err) console.log(err);
+          expect(res).to.have.status(200); //On s'attend au status 200, un objet et que la ficelle soi exacte
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.be.a('string');
+          expect(res.body.message).to.equal('Livre mis à jour');
+          done();
+      });
+})
+
+it('Devrait si le delete a fonctionné envoyer un status 200 et un message',done=>{
+  let message = {
+      message: 'Le livre est bien supprimé' //Instanciation message
+  }
+  nock("http://localhost:8080")
+  .delete('/book/49657845666') //On intercepte la fonction delete et on renvoie le status et le message
+  .reply(200,message)
+      chai
+      .request('http://localhost:8080')
+      .delete('/book/49657845666') 
+      .end((err, res) => { // On vérifie que cela a bien fonctionné avec le status envoyé, et la bonne ficelle 
+          if (err) console.log(err);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.be.a('string');
+          expect(res.body.message).to.equal('Le livre est bien supprimé');
+          done();
+      });
+})
+
+it('Devrait pour le get envoyer un status 200 et un message',done=>{
+  let data = {
+      message: 'Livre récupéré',
+      book : {
+          'id': '5488619',
+          'title': '5 poneys sur une île arrivent ils a construire à radeau pour s enfuir ?',
+          'years': 2018,
+          'pages': 900
+      }
+  }
+  nock("http://localhost:8080")
+  .delete('/book/5488619')
+  .reply(200,data)
+      chai
+      .request('http://localhost:8080')
+      .delete('/book/5488619')
+      .end((err, res) => {
+          if (err) console.log(err);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.be.a('string');
+          expect(res.body.message).to.equal('Livre récupéré');
+          expect(res.body.book).to.be.a('object');
+          expect(res.body.book.years).to.equal(2018);
+          expect(res.body.book.pages).to.equal(900);
+          expect(res.body.book.title).to.equal('5 poneys sur une île arrivent ils a construire à radeau pour s enfuir ?');
+          done();
+      });
+})
+})
+
+
+
+
+describe('Test unity (oups c kassé)', () => {
+
+  beforeEach(()=>{
+      nock.cleanAll() //On clean tout avant le test 
+  })
+  it('Devrait envoyer un status 400 et un message sur le get du tableau de livres',done=>{
+      let message = {
+          message : 'Erreur en récupérant les bouquins, dur'
+      }
+      nock("http://localhost:8080")
+      .get('/book')
+      .reply(400,message) //On intercepte et on envoie notre status et notre message
+          chai
+          .request('http://localhost:8080')
+          .get('/book')
+          .end((err, res) => {
+              if (err) console.log(err);
+              expect(res).to.have.status(400); //On s'attend à 400 un objet une ficelle et que le message soi le même
+              expect(res.body).to.be.a('object');
+              expect(res.body.message).to.be.a('string');
+              expect(res.body.message).to.equal('Erreur en récupérant les bouquins, dur');
+              done();
+          });
+  })
+
+  it('devrait si le post se casse la gueule envoyer une erreur 400 et un message',done=>{
+      let message = {
+          message: 'Erreur en rajoutant le livre'
+      }
+      nock("http://localhost:8080")
+      .post('/book')
+      .reply(400,message)
+          chai
+          .request('http://localhost:8080')
+          .post('/book')
+          .send({
+              "id": "121518796598983",
+              "title": "Si 145 poneys se retrouvent dans le mordor peuvent-ils battre Sauron ?",
+              "years": "3459",
+              "pages": "7893"
+          })
+          .end((err, res) => {
+              if (err) console.log(err); //On s'attend à un status 400 et un texte exact
+              expect(res).to.have.status(400);
+              expect(res.body).to.be.a('object');
+              expect(res.body.message).to.be.a('string');
+              expect(res.body.message).to.equal('Erreur en rajoutant le livre');
+              done();
+          });
+  })
+  it('devrait pour la modif ratée envoyer un 400 et un message',done=>{
+      let message = {
+          message: 'Erreur en essayant de mettre à jour le livre' //Le message 
+      }
+      nock("http://localhost:8080")
+      .put('/book/121518796598983')
+      .reply(400,message) //On envoie le status et le message
+          chai
+          .request('http://localhost:8080')
+          .put('/book/121518796598983')
+          .send({ 
+              "pages": "459" //On envoie la modif
+          })
+          .end((err, res) => {
+              if (err) console.log(err);
+              expect(res).to.have.status(400); 
+              expect(res.body).to.be.a('object'); //Rien ne passe tout est bon
+              expect(res.body.message).to.be.a('string');
+              expect(res.body.message).to.equal('Erreur en essayant de mettre à jour le livre');
+              done();
+          });
+  })
+  it('devrait pour le delete raté envoyer un 400 et le message',done=>{
+      let message = {
+          message: 'Erreur en supprimant le livre'
+      }
+      nock("http://localhost:8080")
+      .delete('/book/121518796598983') //On récupère la fonction pour delete le livre
+      .reply(400,message)
+          chai
+          .request('http://localhost:8080')
+          .delete('/book/121518796598983')
+          .end((err, res) => {
+              if (err) console.log(err);
+              expect(res).to.have.status(400); //On s'attend a un 400 et le message
+              expect(res.body).to.be.a('object');
+              expect(res.body.message).to.be.a('string');
+              expect(res.body.message).to.equal('Erreur en supprimant le livre');
+              done();
+          });
+  })
+
+  it('Devrait pour le get raté envoyer un 400 et le message',done=>{
+      let message = {
+          message: 'Le livre existe pas cousin', //Le message comme d'hab
+      }
+      nock("http://localhost:8080")
+      .delete('/book/121518796598983') //On récupère la fonction delete
+      .reply(400,message) //On lui dit que ça foire direct
+          chai
+          .request('http://localhost:8080')
+          .delete('/book/121518796598983')
+          .end((err, res) => { //Oulala on observe que ça marche pas parce que c'est le même message et status qu'on lui a nous même envoyés
+              if (err) console.log(err)
+              expect(res).to.have.status(400)
+              expect(res.body).to.be.a('object')
+              expect(res.body.message).to.be.a('string')
+              expect(res.body.message).to.equal('Le livre existe pas cousin')
+              done()
+          });
+  })
+})
    /* Chai
     .request(server) 
     .get("/book")
